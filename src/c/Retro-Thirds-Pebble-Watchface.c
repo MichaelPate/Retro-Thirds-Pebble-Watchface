401d17d40f0d03d4eb7f44579ff80caabed0ff93
Michael Pate <pate5800@gmail.com>
2025-12-13 10:32:26 -0700

    background colors displayed with the time

--- src/c/Retro-Thirds-Pebble-Watchface.c
+++ src/c/Retro-Thirds-Pebble-Watchface.c
@@ -9,29 +9,72 @@ static TextLayer *sTimeTextLayer;
 static TextLayer *sDayTextLayer, *sDateTextLayer, *sBattBTTextLayer;
 
 // Our background color layers
-static Layer *sBgTopLayer, *sBgMiddleLayer, *sBgBottomLayer;
-const int top_y = 20, bottom_y = 40; // which y level will the layers be defined at
+static Layer *sBgTopLayer, *sBgBottomLayer; // no background for middle layer since time text just uses its BG color
+#define top_y 54//PBL_IF_ROUND_ELSE(58, 52); // Need to go back
+//const int middle_size = PBL_IF_ROUND_ELSE(58, 52);
+#define bottom_y top_y + 54 // which y level will the layers be defined at
+static GColor bgTopColor, bgMiddleColor, bgBottomColor;
+static GColor weatherTextColor, timeTextColor, dateTextColor;
+
+// Required to set background color of the top and bottom thirds
+static void topThirdBgColor_proc(Layer *layer, GContext *ctx)
+{
+  graphics_context_set_fill_color(ctx, bgTopColor);
+  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
+}
+static void bottomThirdBgColor_proc(Layer *layer, GContext *ctx)
+{
+  graphics_context_set_fill_color(ctx, bgBottomColor);
+  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
+}
+
+// Normally this would go in the tick handler directly,
+// but its its own function so we can also call it whenever we want (init)
+static void updateTime()
+{
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(sTimeTextLayer, s_buffer);
+}
 
 // When the main window first loads
 static void main_window_load(Window *window)
 {
-  // Get information about the Window
+  // Get information about the Window for the watchface
   Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+  GRect windowBounds = layer_get_bounds(window_layer);
 
-  // Create the TextLayer with specific bounds
+  // Create the time TextLayer with specific bounds (in its third of the screen) and colors
   sTimeTextLayer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(sTimeTextLayer, GColorClear);
-  text_layer_set_text_color(sTimeTextLayer, GColorBlack);
+      GRect(0, top_y, windowBounds.size.w, bottom_y - top_y));
+  bgMiddleColor = GColorGreen;
+  timeTextColor = GColorBlack;
+  text_layer_set_background_color(sTimeTextLayer, bgMiddleColor);
+  text_layer_set_text_color(sTimeTextLayer, timeTextColor);
   text_layer_set_text(sTimeTextLayer, "00:00");
   text_layer_set_font(sTimeTextLayer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(sTimeTextLayer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(sTimeTextLayer));
+
+  // Create our background color layers and add them as well
+  sBgTopLayer = layer_create(GRect(0, 0, windowBounds.size.w, top_y));
+  bgTopColor = GColorBlue;
+  layer_set_update_proc(sBgTopLayer, topThirdBgColor_proc);
+  layer_add_child(window_layer, sBgTopLayer);
+
+  sBgBottomLayer = layer_create(GRect(0, bottom_y, windowBounds.size.w, windowBounds.size.h));
+  bgBottomColor = GColorRed;
+  layer_set_update_proc(sBgBottomLayer, bottomThirdBgColor_proc);
+  layer_add_child(window_layer, sBgBottomLayer);
+
+  
 }
 
 // When the main window unloads
@@ -39,6 +82,15 @@ static void main_window_unload(Window *window)
 {
   // Destroy TextLayer
   text_layer_destroy(sTimeTextLayer);
+  layer_destroy(sBgTopLayer);
+  layer_destroy(sBgBottomLayer);
+}
+
+// Tick handler called whenever the time changes
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
+{
+  // The primary purpose of the tick handler is to update the time
+  updateTime();
 }
 
 // Face init code
@@ -55,6 +107,12 @@ static void init()
 
   // Show the Window on the watch, with animated=true
   window_stack_push(sMainWindow, true);
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  // Update the time now so we dont have to wait for the timer service
+  updateTime();
 }
 
 // Face de-init code
